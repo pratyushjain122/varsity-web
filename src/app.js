a6c4a874d6ac11d0d6f8c169799291e7dba35cfb
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-01-14 19:26:56 +0530

    Contact form done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
-// const menuToggle = document.querySelector('.toggle');
-// const showcase = document.querySelector('.showcase');
+const db = firebase.firestore();
+const RefContact = db.collection("Contact");
 
-// menuToggle.addEventListener('click', () => {
-//   menuToggle.classList.toggle('active');
-//   showcase.classList.toggle('active');
-// })
+function handleForm() {
+  const form = document.getElementById("contact");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const enter_name = document.getElementById("enter_name").value;
+    const enter_email = document.getElementById("enter_email").value;
+    const enter_message = document.getElementById("enter_message").value;
+
+    const obj = {
+      Name: enter_name,
+      Email: enter_email,
+      Message: enter_message,
+    };
+
+    RefContact.doc(obj.Email)
+      .set(obj)
+      .then(function () {
+        console.log("Success");
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+
+    form.reset();
+  });
+}
+
+handleForm();

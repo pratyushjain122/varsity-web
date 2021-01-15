8cecc6e33265eefa7cdde7ac3e2d1a3e9209345f
Achyut-0705 <71813671+Achyut-0705@users.noreply.github.com>
2021-01-15 14:11:17 +0530

    gallery added and nav bar adjusted

---
+++ src/js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCw44ei0CUnITN90niop6VGfDWknFr84Po",
+    authDomain: "varsity-web.firebaseapp.com",
+    projectId: "varsity-web",
+    storageBucket: "varsity-web.appspot.com",
+    messagingSenderId: "631498474607",
+    appId: "1:631498474607:web:84e2a3a46270b920f6399a"
+  };
+
+  firebase.initializeApp(firebaseConfig);

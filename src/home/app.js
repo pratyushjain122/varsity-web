a29ebbb7ab412727ba7f741a113b223b09e5f2c6
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-01-17 20:49:38 +0530

    counter animation

--- src/home/app.js
+++ src/home/app.js
@@ -39,18 +39,35 @@ function handleForm() {
 handleForm();
 
 
-$(document).ready(function() {
+var a = 0;
+$(window).scroll(function() {
 
-  $('.counter').each(function () {
-  $(this).prop('Counter',0).animate({
-  Counter: $(this).text()
-  }, {
-  duration: 4000,
-  easing: 'swing',
-  step: function (now) {
-  $(this).text(Math.ceil(now));
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 5000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
   }
-  });
-  });
-  
-  });
+
+});

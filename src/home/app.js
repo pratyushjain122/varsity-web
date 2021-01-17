ca71e1eead06bdc9afc0a6dfcaaafc9f21986815
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-01-17 20:32:56 +0530

    counter added

--- src/home/app.js
+++ src/home/app.js
@@ -37,3 +37,20 @@ function handleForm() {
 }
 
 handleForm();
+
+
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+  $(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+  }, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+  $(this).text(Math.ceil(now));
+  }
+  });
+  });
+  
+  });

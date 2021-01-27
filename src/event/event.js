5d319b4f533a545f46eece77e057a854a1578d6f
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-01-28 04:42:01 +0530

    count down added wrt to 1 event

--- src/event/event.js
+++ src/event/event.js
@@ -13,9 +13,11 @@ async function displayData() {
     console.log(doc.data().Title);
     console.log(doc.data().description);
     console.log(doc.data().event_date);
+    console.log(doc.data().Timestamp);
 
     document.getElementById("upcoming_event_heading").innerHTML = doc.data().Title;
     document.getElementById("upcoming_event_description").innerHTML = doc.data().description;
+    countDownTimer(doc.data().Timestamp);
   });
 }
 
@@ -40,4 +42,34 @@ async function SetImgRef(key) {
         });
       });
     });
+}
+
+function countDownTimer(Timestamp) {
+  // Set the date we're counting down to
+  var countDownDate = Timestamp;
+
+  // Update the count down every 1 second
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML =
+      "LIVE IN : " + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "ALREADY OCCURED!";
+    }
+  }, 1000);
 }

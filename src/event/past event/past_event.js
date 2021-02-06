859176be485bbd43a34fe3996311880b38b02e74
Pratyush Jain <pratyush.jain2001@gmail.com>
2021-02-06 21:59:02 +0530

    past-events JSX + css adjustments

--- src/event/past event/past_event.js
+++ src/event/past event/past_event.js
@@ -3,7 +3,7 @@ const RefCollection = db.collection("Past Event");
 
 console.log("A");
 
-RefCollection.doc("2020")
+RefCollection.doc("2021")
   .collection("events")
   .get()
   .then((querySnapshot) => {
@@ -16,9 +16,17 @@ RefCollection.doc("2020")
         console.log(doc.data());
         const timestamp = new Date(doc.data().Timestamp);
         console.log(timestamp.getFullYear());
-        document.getElementById("post_2021").innerHTML = doc.data().Title;
-        document.getElementById("desc_2021").innerHTML = doc.data().description;
-        document.getElementById("img_2021").src = doc.data().url1;
+        // document.getElementById("post_2021").innerHTML = doc.data().Title;
+        // document.getElementById("desc_2021").innerHTML = doc.data().description;
+        // document.getElementById("img_2021").src = doc.data().url1;
       }
     });
   });
+
+var html_insert =
+  '<div class="col"><div class="card border-dark mb-3" style="max-width: 28rem"><div class="img-card"><img src="../../../assets/past_events/2021_cancer_day.png" id="img_2021" class="card-img-top" alt="..." /></div><div class="card-body text-dark"><h5 class="card-title" id="post_2021">NEW EVENT</h5><p class="card-text" id="desc_2021">brtbtrbtrbrbtrrtbrtbbtrtrbtrbbrtbrtbt</p></div></div></div>';
+
+document.getElementById("container-2021").innerHTML += html_insert;
+// document.getElementById("container-2020").innerHTML += html_insert;
+// document.getElementById("container-2019").innerHTML += html_insert;
+// document.getElementById("container-2018").innerHTML += html_insert;

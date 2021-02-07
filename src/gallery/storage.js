d627ac1a5064bf6c92171f09d1d61a58db547509
Rahul Mansharamani <rahul.mansharamani.btech2019@sitpune.edu.in>
2021-02-07 22:42:54 +0530

    next page & prev page implementation done

--- src/gallery/storage.js
+++ src/gallery/storage.js
@@ -85,44 +85,26 @@ const field = "url";
 const pageSize = 3;
 
 const query = RefCollection.orderBy(field).limit(pageSize);
+
 nextPage();
+
 async function nextPage() {
   //calculate last document of query
 
-  const list_url = [];
+  let list_url = [];
+  console.log(lastdoc);
+
+  if (lastdoc == null) {
+    console.log("null");
+    document.getElementById("previous").style.visibility = "hidden";
+  } else {
+    document.getElementById("previous").style.visibility = "visible";
+  }
+
   const ref = RefCollection.orderBy("createdAt")
     .startAfter(lastdoc || 0)
     .limit(6);
-  const data = await ref.get();
 
-  data.docs.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    //console.log(doc.id, " => ", doc.data());
-    list_url.push(doc.data().url);
-  });
-  lastdoc = data.docs[data.docs.length - 1];
-
-  console.log(list_url);
-  console.log(lastdoc);
-
-  var start = no_image * (page - 1);
-  var end = no_image * page;
-  document.getElementById("image-container").innerHTML = "";
-  for (var x = 0; x < list_url.length; x++) {
-    var html_insert = '<div class="item col-md-4"><img class="image" src="' + list_url[x] + '" alt="gallery_image"></div>';
-    console.log(html_insert);
-    document.getElementById("image-container").innerHTML += html_insert;
-  }
-}
-
-async function prevPage() {
-  //calculate first document of query
-  //display();
-  //return RefCollection.orderBy("createdAt").endBefore(first[field]).limitToLast(6);
-
-  const list_url = [];
-
-  const ref = RefCollection.orderBy("createdAt").endAt(firstdoc).limitToLast(6);
   const data = await ref.get();
 
   data.docs.forEach((doc) => {
@@ -133,22 +115,38 @@ async function prevPage() {
   lastdoc = data.docs[data.docs.length - 1];
   firstdoc = data.docs[0];
   console.log(list_url);
-  console.log(firstdoc);
+  console.log(lastdoc);
 
+  print_image(list_url);
+}
+
+async function prevPage() {
+  //calculate first document of query
+
+  let list_url = [];
+  console.log(firstdoc);
+  const ref = RefCollection.orderBy("createdAt").endBefore(firstdoc).limitToLast(6);
+  const data = await ref.get();
+
+  data.docs.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    //console.log(doc.id, " => ", doc.data());
+    list_url.push(doc.data().url);
+  });
+  lastdoc = data.docs[data.docs.length - 1];
+  firstdoc = data.docs[0];
+  console.log(list_url);
+  console.log(lastdoc);
+  print_image(list_url);
+}
+
+async function print_image(list_url) {
   var start = no_image * (page - 1);
   var end = no_image * page;
   document.getElementById("image-container").innerHTML = "";
   for (var x = 0; x < list_url.length; x++) {
     var html_insert = '<div class="item col-md-4"><img class="image" src="' + list_url[x] + '" alt="gallery_image"></div>';
-    console.log(html_insert);
+    //console.log(html_insert);
     document.getElementById("image-container").innerHTML += html_insert;
   }
 }
-
-// const timestamp = Date.now();
-
-// RefCollection.doc("image 12.jpg")
-//   .update({ createdAt: timestamp })
-//   .then(function () {
-//     console.log("sucess");
-//   });
